f8321bfe51eb20fd8390575eabd4948cc5c524ab
Rx-de <Rx-de@github.com>
2025-02-28 22:15:05 +0700

    Adeed: ksu drivers
    Signed-off-by: Rx-de <Rx-de@github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1870,6 +1870,14 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
@@ -1884,6 +1892,12 @@ int do_execveat(int fd, struct filename *filename,
 		const char __user *const __user *__envp,
 		int flags)
 {
+ 	#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	#endif
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 
